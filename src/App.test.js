6cd3d654298645619641caf91eb392e0e11bc471
rishi <rishikesh.sirigiripet@bilvantis.io>
2025-01-13 17:04:20 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/appleee/i);
+  const linkElement = screen.getByText(/balll/i);
   expect(linkElement).toBeInTheDocument();
 });
