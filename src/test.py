c57d60f1b229b82e76b6cc01c9325409613a8484
rishi <rishikesh.sirigiripet@bilvantis.io>
2025-01-22 11:22:59 +0530

    test

--- src/test.py
+++ src/test.py
@@ -1,6 +1,5 @@
 print("hihfujkmkgi")
 print("hisdfbhk")
 
-fgjfgok
-scvbnjuj Zfdxlijkv
+fgjfgokjuj Zfdxlijkv
 OPENAI_API_KEY=proj-emwNFuV3XKhJEK7RSdXKrtP72LaTWqEtn_tpLF4EvZo396Olto-a3VZxFfdZeme0M0q0pj11dRT3BlbkFJOKhdXrYDaBYkTjMKLHgmgvgoVLb3f1Dsq2D_fErLylQmI7-YKzuzB6MsPQMzv0dlL-LVpTCFoA
