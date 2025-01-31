f6640d61e964e734f01e93760b409631703da34e
rishi <rishikesh.sirigiripet@bilvantis.io>
2025-01-31 16:39:54 +0530

    test

--- src/test.py
+++ src/test.py
@@ -1,7 +1,5 @@
 print("hihfujkmkgi")
 print("hisdfbhk")
 
-
-
-fgjfgokju j Zfdxlijkvapi_url="http://0.0.0.0:3000/review/cvbnflkc
-OPENAI_API_KEY=proj-emwNFuV3XKhJEK7RSdXKrtP72LaTWqEtn_tpLF4EvZo396Olto-a3VZxFfdZeme0M0q0pj11dRT3BlbkFJOKhdXrYDaBYkTjMKLHgmgvgoVLb3f1Dsq2D_fErLylQmI7-YKzuzB6MsPQMzv0dlL-LVpTCFoA
+def add_numbers(a, b):
+    return a + b
